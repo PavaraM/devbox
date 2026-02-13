df027c464a59568d7ca427ee7ebedf1184f6c4d2
PavaraM <pavaramirihagalla@gmail.com>
2026-02-14 00:23:07 +0530

    fixed variable bugs

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,7 +1,7 @@
 # lib/logging.sh
 # This file contains logging functions and setup for the devbox script.
 
-logfile="$SCRIPT_DIR/logs/devbox_$timestamp.log"
+logfile="$SCRIPT_DIR/logs/devbox_$TIMESTAMP.log"
 mkdir -p logs/
 
 #logfile header
@@ -11,9 +11,9 @@ echo "------------------------------" >> "$logfile"
 
 log_footer() {
     local exit_code=$?
-    end_time=$(date +%s%3N)
+    END_TIME=$(date +%s%3N)
     
-    duration_ms=$((end_time - start_time))
+    duration_ms=$((END_TIME - START_TIME))
     duration_s=$(awk "BEGIN {printf \"%.3f\", $duration_ms/1000}")
     echo "------------------------------" >> "$logfile"
     echo "Script ended at $(date) exit_code=$exit_code duration=${duration_s}s" >> "$logfile"
