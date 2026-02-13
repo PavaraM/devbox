7833993cedde9f04992df43d6cf2faab8a608e0c
PavaraM <pavaramirihagalla@gmail.com>
2026-02-13 22:21:04 +0530

    created a seperate script for logging fuctions

---
+++ lib/logging.sh
@@ -0,0 +1,28 @@
+# lib/logging.sh
+logfile="./logs/devbox_$timestamp.log"
+mkdir -p logs/
+
+#logfile header
+echo "script started at $(date)" >> "$logfile"
+echo "command: devbox $@" >> "$logfile"
+echo "------------------------------" >> "$logfile"
+
+log_footer() {
+    local exit_code=$?
+    end_time=$(date +%s%3N)
+    
+    duration_ms=$((end_time - start_time))
+    duration_s=$(awk "BEGIN {printf \"%.3f\", $duration_ms/1000}")
+    echo "------------------------------" >> "$logfile"
+    echo "Script ended at $(date) exit_code=$exit_code duration=${duration_s}s" >> "$logfile"
+    echo "==============================" >> "$logfile"
+}
+
+log() {
+    local level=$1
+    shift
+    local line="$(date +%H:%M:%S) [$level] $*"
+    
+    #echo "$line"               # console
+    echo "$line" >> "$logfile"  # file
+}
