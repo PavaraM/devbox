d25bf8899d0468c9e006cf2ece5e2e15f1abaccf
PavaraM <pavaramirihagalla@gmail.com>
2026-02-13 23:55:55 +0530

    Fix logfile path to use SCRIPT_DIR for consistency

--- lib/logging.sh
+++ lib/logging.sh
@@ -1,7 +1,7 @@
 # lib/logging.sh
 # This file contains logging functions and setup for the devbox script.
 
-logfile="./logs/devbox_$timestamp.log"
+logfile="$SCRIPT_DIR/logs/devbox_$timestamp.log"
 mkdir -p logs/
 
 #logfile header
